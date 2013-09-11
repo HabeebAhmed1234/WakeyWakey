3c83a8c1f76cfb52643f98c5b0b417eb47614070
Hashim Mir <hashim.mir123@gmail.com>
2013-09-10 20:46:12 -0400

    fixed green bar bug 3

--- src/com/example/alarmclock/CustomImageView.java
+++ src/com/example/alarmclock/CustomImageView.java
@@ -35,9 +35,6 @@ public class CustomImageView extends ImageView {
 	    float fullHeight = (float) (canvas.getHeight()*0.1 - base);
 	    float percentage = alhi.getPercentage();
 
-	    paint.setColor(getResources().getColor(R.color.Black));
-	    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), paint);
-	    
 	    // left, top, right, bottom
 	    paint.setColor(getResources().getColor(R.color.batteryJuice));
 	    canvas.drawRect(midX - width, (float) (base+percentage*fullHeight), midX + width, base, paint);
